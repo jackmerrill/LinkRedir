b2421214a36246a75228f2055d636b096746f67b
Jack Merrill <me@jackmerrill.com>
2020-01-15 19:20:07 -0600

    added api (/api)

--- server.py
+++ server.py
@@ -56,5 +56,49 @@ def goTo(link):
         return "Wrong link lol"
     return render_template("redir.html", metatitle=checkLink.metatitle, metadesc=checkLink.metadesc, metaurl=checkLink.metaurl, metaimg=checkLink.metaimg, redir=checkLink.redir)
 
+@app.route("/api")
+def api_base():
+    return """
+        I'm too lazy to make a webpage for this
+        <br>
+        DOCS:
+        <br>
+        /api/new - POST Request with headers:
+        <br>
+        shortlink*  -  The shortlink
+        <br>
+        url*  -  The URL to redirect to
+        <br>
+        metatitle|desc|url|img  -  Meta tags
+        <br>
+        * = required
+    """
+
+@app.route("/api/new", methods=["POST"])
+def api_new():
+    shortlink = request.headers["shortlink"]
+    url = request.headers["url"]
+    if request.headers['metatitle']:
+        metatitle = request.headers['metatitle']
+    else:
+        metatitle=None
+    if request.headers['metadesc']:
+        metadesc = request.headers['metadesc']
+    else:
+        metadesc=None
+    if request.headers['metaurl']:
+        metaurl = request.headers['metaurl']
+    else:
+        metaurl=None
+    if request.headers['metaimg']:
+        metaimg = request.headers['metaimg']
+    else:
+        metaimg=None
+    newLink = Links(link=shortlink, redir=url, metatitle=metatitle, metadesc=metadesc, metaurl=metaurl, metaimg=metaimg)
+    db.session.add(newLink)
+    db.session.commit()
+    return "https://go.jackmerrill.com/{0}".format(shortlink)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969, debug=True)
