2a93cc10d487f582f0d472365334f951c50d67f7
Jonathan Merrill <Merri365@WTHS.NET>
2020-01-15 18:44:53 -0600

    added meta tags

--- server.py
+++ server.py
@@ -10,13 +10,40 @@ class Links(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(512))
     redir = db.Column(db.String(1024))
+    metatitle = db.Column(db.String(512))
+    metadesc = db.Column(db.String(512))
+    metaurl = db.Column(db.String(512))
+    metaimg = db.Column(db.String(512))
 
 db.create_all()
 
 @app.route('/', methods=["POST", "GET"])
 def hello():
     if request.method == "POST":
-        newLink = Links(link=request.form["weblink"], redir=request.form["link"])
+        metatitle=None
+        metadesc=None
+        metaurl=None
+        metaimg=None
+        if request.form['metatitle']:
+            metatitle = request.form['metatitle']
+        else:
+            metatitle=None
+        if request.form['metadesc']:
+            metadesc = request.form['metadesc']
+        else:
+            metadesc=None
+        if request.form['metaurl']:
+            metaurl = request.form['metaurl']
+        else:
+            metaurl=None
+        if request.form['metaimg']:
+            metaimg = request.form['metaimg']
+        else:
+            metaimg=None
+
+        # haha bad code
+
+        newLink = Links(link=request.form["weblink"], redir=request.form["link"], metatitle=metatitle, metadesc=metadesc, metaurl=metaurl, metaimg=metaimg)
         db.session.add(newLink)
         db.session.commit()
         return render_template("index.html", alert='Success! Link is https://go.jackmerrill.com/{0}').format(request.form["weblink"])
@@ -27,7 +54,7 @@ def goTo(link):
     checkLink = Links.query.filter_by(link=link).first()
     if not checkLink:
         return "Wrong link lol"
-    return redirect(checkLink.redir)
+    return render_template("redir.html", metatitle=checkLink.metatitle, metadesc=checkLink.metadesc, metaurl=checkLink.metaurl, metaimg=checkLink.metaimg, redir=checkLink.redir)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6969, debug=False)
+    app.run(host="0.0.0.0", port=6969, debug=True)
